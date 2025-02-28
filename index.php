4d2932aab0826d985c23e2f1250086733f3a2158
MarcoProstamo <Marcopros1201@gmail.com>
2025-02-28 17:07:12 +0100

    Add → Movie Class

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+
+    class Movie{
+        public $title;
+        public $director;
+        public $release_year;
+        protected $budget;
+
+        public function __construct($_title, $_director, $_release_year, $_budget){
+            $this->title = $_title;
+            $this->director = $_director;
+            $this->release_year = $_release_year;
+            $this->budget = $_budget;
+        }
+        
+        public function getBudget(){
+            return $this->budget;
+        }
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
